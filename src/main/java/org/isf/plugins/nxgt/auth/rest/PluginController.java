acf3727279451abdafb437db9f055d92c5ef0303
Steve Tsala <stevegires@gmail.com>
2026-03-03 09:08:09 +0100

    update: Update config

--- src/main/java/org/isf/plugins/nxgt/auth/rest/PluginController.java
+++ src/main/java/org/isf/plugins/nxgt/auth/rest/PluginController.java
@@ -8,19 +8,41 @@ import org.isf.plugins.nxgt.openapi.model.SearchRequest;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.isf.plugins.nxgt.auth.port.IPermissionService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
 
-@RestController("PermissionController")
-@Tag(name = "NXGT Permissions")
-@RequestMapping(value = "/plugins/nxgt/permissions")
-public class PermissionController {
+@RestController
+@Tag(name = "Plugins")
+@RequestMapping(value = "/plugins")
+public class PluginController {
 	final IPermissionService service;
 
-	public PermissionController(IPermissionService service) {
+	public PluginController(IPermissionService service) {
 		this.service = service;
+	}
+
+	@GetMapping
+	public List<String> findAllPlugins() {
+		return Collections.emptyList();
+	}
+
+	@PostMapping("/{id}/{endpoint}")
+	public Object invokePostEndpoint(
+		@PathVariable String id,
+		@PathVariable String endpoint,
+		@RequestBody(required = false) Object body
+	) {
+		return service.invokeEndpoint(id, endpoint, "POST", body);
+	}
+
+	@GetMapping("/{id}/{endpoint}")
+	public Object invokePostEndpoint(
+		@PathVariable String id,
+		@PathVariable String endpoint
+	) {
+		return service.invokeEndpoint(id, endpoint, "GET", null);
 	}
 
 	@PostMapping("/search")
